193cf2bc272db7905a6646e2b79ed7ee3dd384a6
Ashmianuroop <41049808+Ashmianuroop@users.noreply.github.com>
2018-09-10 09:20:57 +0530

    Add files via upload

---
+++ python8.py
@@ -0,0 +1,11 @@
+c = int(input("Enter the value of n: "))
+hold = c
+sum = 0
+
+if c <= 0: 
+   print("Enter a whole positive number!") 
+else: 
+   while c > 0:
+        c = c + num
+        c = c - 1;
+    print("Sum of first", hold, "natural numbers is: ", sum)
