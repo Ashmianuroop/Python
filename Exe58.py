b9c808d52dce102bbbbe22de6b7305aa47379081
Ashmianuroop <41049808+Ashmianuroop@users.noreply.github.com>
2019-01-07 13:51:34 +0530

    Create Exe58.py

---
+++ Exe58.py
@@ -0,0 +1,10 @@
+e=int(input("enter the no"))
+r=int(input("enter the no"))
+j=[]
+for i in range(0,e):
+    c=int(input("enter"))
+    r.append(c)
+if(r in j):
+    print("yes")
+else:
+    print("no")
