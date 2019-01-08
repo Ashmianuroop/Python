e57cfce8b67e39996fdbd72787772667789769ee
Ashmianuroop <41049808+Ashmianuroop@users.noreply.github.com>
2019-01-08 12:01:01 +0530

    Create python72.py

---
+++ python72.py
@@ -0,0 +1,6 @@
+string1=str(input(''))
+for x in string1:
+    if(x=='A'or x=='a'or x=='E' or x=='e' or x=='I' or x=='i' or x=='O' or x=='o' or x=='U' or x==' u'):
+        print('yes')
+    else:
+        print('no')
